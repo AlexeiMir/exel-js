09b81ab77a52e6d617e67da6faed4814f5a486be
AlexeiMir <alex_16.07@mail.ru>
2020-10-15 14:40:11 +0700

    'class_dom'

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -2,7 +2,7 @@ import {$} from '@core/dom'
 
 export class Excel {
     constructor(selector, options) {
-        this.$el = document.querySelector(selector)
+        this.$el = $(selector)
         this.components = options.components || []
     }
 
@@ -11,16 +11,13 @@ export class Excel {
         this.components.forEach(Component => {
             const $el = $.create('div', Component.className)
                 const component = new Component($el)
-                $el.innerHTML = component.toHTML()
+                $el.html(component.toHTML())
                 $root.append($el)
             }
         )
         return $root
     }
-
-
     render() {
-        // this.$el.insertAdjacentHTML('afterbegin',`<h1>Test</h1>`)
         this.$el.append(this.getRoot())
     }
 }
