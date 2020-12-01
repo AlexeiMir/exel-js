35f293ade5d44fee8ba876f6d85175fa8c93b12f
AlexeiMir <alex_16.07@mail.ru>
2020-12-01 17:35:43 +0700

    Finish redux and table logic

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,25 +1,25 @@
 import {$} from '@core/dom'
 import {Emitter} from '@/components/table/Emitter';
+import {StoreSubscriber} from '@core/StoreSubscriber';
 
 export class Excel {
     constructor(selector, options) {
         this.$el = $(selector)
         this.components = options.components || []
         this.emitter = new Emitter()
+        this.store = options.store
+        this.subscriber = new StoreSubscriber(this.store)
     }
 
     getRoot() {
         const $root = $.create('div', 'excel')
         const componentOptions = {
-            emitter: this.emitter
+            emitter: this.emitter,
+            store: this.store
         }
         this.components = this.components.map(Component => {
                 const $el = $.create('div', Component.className)
                 const component = new Component($el, componentOptions)
-                // DEBUG
-               /* if (component.name) {
-                    window['c' + component.name] = component
-                }*/
                 $el.html(component.toHTML())
                 $root.append($el)
                 return component
@@ -30,9 +30,11 @@ export class Excel {
 
     render() {
         this.$el.append(this.getRoot())
+        this.subscriber.subscribeComponents(this.components)
         this.components.forEach(component => component.init())
     }
     destroy() {
+        this.subscriber.unSubscribeComponents()
         this.components.forEach(component => component.destroy())
     }
 }
--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -7,6 +7,7 @@ export class Formula extends ExcelComponent {
         super($root, {
             name: 'Formula',
             listeners: ['input', 'keydown'],
+            subscribe: ['currentText'],
             ...options
         });
     }
@@ -19,9 +20,13 @@ export class Formula extends ExcelComponent {
     }
     init() {
         super.init()
-        const $formula = this.$root.find('#formula')
-        this.$on('table:input', ($cell) => $formula.text($cell.text()))
-        this.$on('table-select', ($cell) => $formula.text($cell.text()))
+        this.$formula = this.$root.find('#formula')
+        this.$on('table-select', ($cell) => this.$formula
+            .text($cell.data.value))
+    }
+    storeChanged({currentText}) {
+        console.log('Formula changed', currentText)
+        this.$formula.text(currentText)
     }
     onInput(event) {
         this.$emit('formula:input', $(event.target).text())
